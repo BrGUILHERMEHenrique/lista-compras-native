c9209778b32e61a6b45011f5613dd534abbcfef3
BrGUILHERMEHenrique <guilhermehenriquersouza17@gmail.com>
2022-09-16 23:33:00 -0300

    Criando interceptors da requisição

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,20 @@
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const api = axios.create({
   baseURL: "https://api-java-lista.herokuapp.com/"
+});
+
+api.interceptors.request.use(async (req) => {
+  const token = await AsyncStorage.getItem('@Lista:token');
+
+  if(token){
+    req.headers = {
+      'Authorization': token
+    }
+  }
+
+  return req;
 });
 
 export default api;
